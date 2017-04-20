611be63ddeee5e7018b74facbaf94a866da5390e
Ewelina Dziewiela <ewelinadziewiela@MacBook-Pro-Ewelina.local>
2017-04-21 00:32:33 +0200

    article changes + cookies alert

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
-$(".menu-icon").click(function() {
+$(".menu-icon").click(function () {
     $(".navbar-mobile").toggleClass("hidden");
+});
+
+
+$(document).ready(function() {
+
+
+    var cookieAlert = $('<div class="cookie-alert">' +
+        '<p class="cookie-alert__text">Zbieramy cookies cokolwiek to znaczy.' +
+        '<a href="" class="cookie-alert__policy">Akceptujesz naszą politykę prywatności i cookies?</a></p>' +
+        '<p class="cookie-alert__btn"><a class="cookie-alert__link">akceptuję!</a></p>' +
+        '</div>'
+        );
+
+    $(".cookie-alert__wrapper").append(cookieAlert);
+
+    $(".cookie-alert__btn").click(function() {
+        $(".cookie-alert__wrapper").children(cookieAlert).remove();
+
+
+    })
+
 });
